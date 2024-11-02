11cc1fb96a800e0f58cf8c91548cbf0423cdcfb6
Aleksei Bukhalov <Buhalov.alexei@yandex.ru>
2024-11-02 15:35:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Указываем папку для статических файлов
+app.use(express.static('public'));
+
+// Главный маршрут
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/main.html'));
+});
+
+// Запускаем сервер
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
