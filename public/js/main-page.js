495b0205d659868fa851530af1881afda22ee525
bukhalov1 <buhalov.alexei@yandex.ru>
2025-02-02 20:56:15 +0300

    new

---
+++ public/js/main-page.js
@@ -0,0 +1,11 @@
+
+document.querySelector('.airdrop-page').addEventListener('click', function() {
+    // Находим ссылку и выполняем переход
+    window.location.href = 'game.html';
+});
+
+
+document.querySelector('.task-page').addEventListener('click', function() {
+    // Находим ссылку и выполняем переход
+    window.location.href = 'main.html';
+});
